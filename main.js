6d9284790dc8b523a092cfa61df046c305249f4c
Alina bc2025-4 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-03-10 14:16:17 +0200

    modified main.js

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const {program} = require('commander');
 const http = require('http');
+const fs = require('fs').promises;
 
 program
-	.requiredoption('-i, --input <path>', 'path to input file')
-	.requiredoption('-h, --host <addres>', 'server address')
-	.requiredoption('-p, --port <port>', 'server port');
+	.requiredOption('-i, --input <path>', 'path to input file')
+	.requiredOption('-h, --host <addres>', 'server address')
+	.requiredOption('-p, --port <port>', 'server port');
 
 program.parse(process.argv);
 
 const options = program.opts();
 
+try { 
+fs.access(options.input); } 
+catch { 
+console.error ('Cannot find input file');
+process.exit(1); }
+
+let server = http.createServer();
+server.listen(options.port, options.host, callback);
