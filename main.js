0289094daab012ed173bfaada298b818012c2195
Alina bc2025-4 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-03-12 13:22:36 +0200

    trying something with this json data

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { program } = require('commander');
 const http = require('http');
 const fs = require('fs').promises;
+const url = require('url');
 
 program
 	.requiredOption('-i, --input <path>', 'path to input file')
@@ -24,10 +25,22 @@ async function Check_file(path) {
 async function Start_server() {
 	await Check_file(options.input);
 	let server = http.createServer(async (req, res) => {
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
-		res.end("Server is alive");
+		res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+		//res.end("Server is alive");
+		const fileContent = await fs.readFile(options.input, 'utf8');
+		const banks = JSON.parse(fileContent);
+		const parsed_url = url.parse(req.url, true);
+		const query = parsed_url.query;
+		let result = banks;
+
+		if (query.normal === 'true') {
+			result = banks.filter(b => b.COD_STATE === 1);
+		}
+
+		res.end(JSON.stringify(result));
+
 	});
-	server.listen(options.port, options.host, ()=> console.log("Server is listening"));
+	server.listen(options.port, options.host, () => console.log("Server is listening"));
 }
 
 Start_server().catch(err => console.error("error starting server", err));
