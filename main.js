e75748e45b2c06c374f3bd14426b9d717ff880af
Alina bc2025-4 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-03-12 12:52:55 +0200

    done writing part1, simple server is working

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-const {program} = require('commander');
+const { program } = require('commander');
 const http = require('http');
 const fs = require('fs').promises;
 
 program
 	.requiredOption('-i, --input <path>', 'path to input file')
 	.requiredOption('-h, --host <addres>', 'server address')
-	.requiredOption('-p, --port <port>', 'server port');
+	.requiredOption('-p, --port <port>', 'server port', parseInt);
 
 program.parse(process.argv);
 
 const options = program.opts();
 
-try { 
-fs.access(options.input); } 
-catch { 
-console.error ('Cannot find input file');
-process.exit(1); }
+async function Check_file(path) {
+	try {
+		await fs.access(path);
+	}
+	catch {
+		console.error('Cannot find input file');
+		process.exit(1);
+	}
+}
 
-let server = http.createServer();
-server.listen(options.port, options.host, callback);
+async function Start_server() {
+	await Check_file(options.input);
+	let server = http.createServer(async (req, res) => {
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end("Server is alive");
+	});
+	server.listen(options.port, options.host, ()=> console.log("Server is listening"));
+}
+
+Start_server().catch(err => console.error("error starting server", err));
