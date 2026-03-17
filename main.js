48a380919f7a0596eadca1dd75184d411c6344b9
Alina bc2025-4 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-03-17 12:44:48 +0200

    added part2 code to mainjs, lab is done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { program } = require('commander');
 const http = require('http');
 const fs = require('fs').promises;
 const url = require('url');
+const { XMLBuilder } = require('fast-xml-parser');
 
 program
 	.requiredOption('-i, --input <path>', 'path to input file')
@@ -25,20 +26,30 @@ async function Check_file(path) {
 async function Start_server() {
 	await Check_file(options.input);
 	let server = http.createServer(async (req, res) => {
-		res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+		res.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' });
 		//res.end("Server is alive");
 		const fileContent = await fs.readFile(options.input, 'utf8');
 		const banks = JSON.parse(fileContent);
 		const parsed_url = url.parse(req.url, true);
-		const query = parsed_url.query;
 		let result = banks;
 
-		if (query.normal === 'true') {
+		if (parsed_url.query.normal === 'true') {
 			result = banks.filter(b => b.COD_STATE === 1);
 		}
+		const bankList = result.map((bank) => {
+			const entry = {};
 
-		res.end(JSON.stringify(result));
+			if (parsed_url.query.mfo === 'true') {
+				entry.mfo_code = bank.MFO;
+			}
+			entry.name = bank.SHORTNAME;
+			entry.state_code = bank.COD_STATE;
+			return entry;
+		});
+		const builder = new XMLBuilder({ arrayNodeName: 'bank', format: true});
+		const xml = builder.build({ banks: { bank: bankList } });
 
+		res.end(xml);
 	});
 	server.listen(options.port, options.host, () => console.log("Server is listening"));
 }
